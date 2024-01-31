17eb20dc769d6e416d08b4be238458c9fbae07d8
P Avinash <avinash.r@theswipewire.in>
2024-01-31 19:28:03 +0530

    locationservice issue

--- src/app/client-location.service.ts
+++ src/app/client-location.service.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ClientLocation } from './client-location';
-import { JsonPipe } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
